6596fd60681e45aaaa49a5c17f297f5dfd585bb7
Crowdbotics <team@crowdbotics.com>
2023-07-08 01:14:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-cherry-41992",
+  url: "https://polished-cherry-41992.botics.co",
+  api: "https://polished-cherry-41992.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
